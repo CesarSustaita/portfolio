0e2c2d5add6da43bd0046253a9d1c4654d9d5023
Cesar Sustaita <c.sustaita@outlook.com>
2024-08-03 14:27:40 -0600

    refactor: render card projects using an array

--- src/components/workContainer/MyWork.vue
+++ src/components/workContainer/MyWork.vue
@@ -1,41 +1,47 @@
-<script>
-import CardProject from '@/components/workContainer/CardsProject/CardProject.vue';
+<script setup>
+import CardProject from '@/components/CardsProject/CardProject.vue'
 
-export default {
-    components: {
-        CardProject
-    }
-}
-
+const PROJECTS = [
+  {
+    name: 'Project 1'
+  },
+  {
+    name: 'Project 2'
+  },
+  {
+    name: 'Project 3'
+  },
+  {
+    name: 'Project 4'
+  },
+  {
+    name: 'Project 5'
+  },
+  {
+    name: 'Project 6'
+  }
+]
 </script>
 
 <template>
-
-    <section class="title">My Work</section>
-    <section class="grid">
-        <CardProject/>
-        <CardProject/>
-        <CardProject/>
-        <CardProject/>
-        <CardProject/>
-        <CardProject/>
-    </section>
-
+  <section class="title">My Work</section>
+  <section class="grid">
+    <CardProject v-for="project in PROJECTS" :key="project.name" />
+  </section>
 </template>
 
-<style scoped> 
-
+<style scoped>
 .title {
-   font-size: 36px;
-   font-weight: 400; 
-   margin-top: 30px;
+  font-size: 36px;
+  font-weight: 400;
+  margin-top: 30px;
 }
 
 .grid {
-    display: flex;
-    flex-wrap: wrap;
-    margin-top: 40px;
-    margin-bottom: 60px;
-    gap: 28px;
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 40px;
+  margin-bottom: 60px;
+  gap: 28px;
 }
 </style>
