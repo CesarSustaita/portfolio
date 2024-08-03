c078018ce9f38b89d86f3bcb80f01a01c9d95b2f
Cesar Sustaita <c.sustaita@outlook.com>
2024-08-03 14:11:49 -0600

    style: format router file

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import WorkView from '../views/WorkView.vue'
+import WorkView from '@/views/WorkView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,8 +8,7 @@ const router = createRouter({
       path: '/',
       name: 'work',
       component: WorkView
-    },
-    
+    }
   ]
 })
 
