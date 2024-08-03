e22963ed88fb13e035724b72fe2cb6c3ed43bb0d
Cesar Sustaita <c.sustaita@outlook.com>
2024-08-03 14:26:40 -0600

    refactor: use script setup in work section

--- src/components/workContainer/workSection.vue
+++ src/components/workContainer/workSection.vue
@@ -1,87 +1,69 @@
-<script>
-import greetings from '@/components/workContainer/greetingsArray.vue';
-import IntroductionPost from '@/components/workContainer/IntroductionPost.vue';
-import MyWork from '@/components/workContainer/MyWork.vue';
-
-
-export default {
-    components: {
-        greetings,
-        IntroductionPost,
-        MyWork
-    }
-}
-
+<script setup>
+import GreetingsArray from '@/components/WorkContainer/GreetingsArray.vue'
+import IntroductionPost from '@/components/WorkContainer/IntroductionPost.vue'
+import MyWork from '@/components/WorkContainer/MyWork.vue'
 </script>
 
 <template>
-    <section class="container">
-        <greetings/>
-        <IntroductionPost/>
-        <div class="line"></div>
-        <MyWork/>
-    </section>
+  <section class="container">
+    <GreetingsArray />
+    <IntroductionPost />
+    <span class="line"></span>
+    <MyWork />
+  </section>
 </template>
 
 <style scoped>
-
-.container{
-    margin-top: 24px;
-    display: flex;
-    flex-direction: column;
-    min-height: 100vh;
-    border-radius: 20px;
-    background-color: rgba(58, 56, 56, 0.28);
-    backdrop-filter: blur(30px);
-    border: 1px solid rgba(140, 140, 140, 0.50);
-    padding-top: 80px;
-    padding-left: 100px;
-    padding-right: 100px;
-
+.container {
+  margin-top: 24px;
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  border-radius: 20px;
+  background-color: rgba(94, 94, 94, 0.164);
+  backdrop-filter: blur(50px);
+  border: 1px solid rgba(140, 140, 140, 0.5);
+  padding-top: 80px;
+  padding-left: 100px;
+  padding-right: 100px;
 }
 
 .line {
-    border-bottom: 1px solid #8C8C8C;
-    margin-top: 60px;
+  border-bottom: 1px solid #8c8c8c;
+  margin-top: 60px;
 }
 
-@media (max-width: 1400px) { 
-
-.container{
+@media (max-width: 1400px) {
+  .container {
     padding-top: 40px;
     padding-left: 60px;
     padding-right: 60px;
-}
+  }
 }
 
 @media (max-width: 900px) {
-
-.container{
+  .container {
     padding-top: 20px;
     padding-left: 30px;
     padding-right: 30px;
-}
+  }
 
-.line {
-    border-bottom: 1px solid #8C8C8C;
+  .line {
+    border-bottom: 1px solid #8c8c8c;
     margin-top: 50px;
-}
-
+  }
 }
 
 @media (max-width: 700px) {
-
-.container{
+  .container {
     padding-top: 15px;
     padding-left: 24px;
     padding-right: 24px;
-}
+  }
 
-.line {
-    border-bottom: 1px solid #8C8C8C;
+  .line {
+    border-bottom: 1px solid #8c8c8c;
     margin-top: 40px;
+  }
 }
-
-}
-
 </style>
