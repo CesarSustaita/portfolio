dc6698c4f8209508436e0ead27ad07773d8b0daa
Cesar Sustaita <c.sustaita@outlook.com>
2024-08-03 14:22:38 -0600

    refactor: use script setup in navbar main

--- src/components/navbar/navbarMain.vue
+++ src/components/navbar/navbarMain.vue
@@ -1,54 +1,36 @@
-<script>
-
-import navControls from '@/components/navbar/navbarControls.vue';
-import buttonLinks from '@/components/navbar/buttonLinks.vue';
-
-export default {
-    components: {
-        navControls,
-        buttonLinks
-    }
-}
-
+<script setup>
+import navControls from '@/components/navbar/navbarControls.vue'
+import buttonLinks from '@/components/navbar/buttonLinks.vue'
 </script>
 
 <template>
-
-<section class="navbar">
+  <section class="navbar">
     <section class="titleLogo">César Sustaita</section>
-    <section class="btnControls"><navControls/></section>
-    <section class="btnLinks"><buttonLinks/></section>
-
-</section>
-
+    <section class="btnControls"><navControls /></section>
+    <section class="btnLinks"><buttonLinks /></section>
+  </section>
 </template>
 
-<style scoped> 
+<style scoped>
 .navbar {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    height: 44px;
-    border: 0px solid red;
-    margin-top: 46px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 44px;
+  border: 0px solid red;
+  margin-top: 46px;
 }
 
-.titleLogo{
-    font-size: 24px;
-    font-weight: 400;
-    letter-spacing: 3.5px;
-    width: 214px;
+.titleLogo {
+  font-size: 24px;
+  font-weight: 400;
+  letter-spacing: 3.5px;
+  width: 214px;
 }
-
 
 @media (max-width: 900px) {
-
-.titleLogo{
+  .titleLogo {
     display: none;
+  }
 }
-}
-
-
-
-
 </style>
