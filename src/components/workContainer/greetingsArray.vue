841c9fb2f81023cae4f4b89ebade91398d738210
Cesar Sustaita <c.sustaita@outlook.com>
2024-08-03 14:26:59 -0600

    refactor: simplify greetings array

--- src/components/workContainer/greetingsArray.vue
+++ src/components/workContainer/greetingsArray.vue
@@ -1,69 +1,62 @@
-<script>
-import { ref, onMounted, onUnmounted } from 'vue';
+<script setup>
+import { ref, onMounted, onUnmounted } from 'vue'
 
-export default{
-setup(){
-    const greetings = [
-    "Hola",
-    "Hi",
-    "Bonjour",
-    "Ciao",
-    "Привет",
-    "こんにちは",
-    "Hei",
-    "Merhaba",
-    "Hallo",
-    "مرحبا"
-    ]
+const GREETINGS = [
+  'Hola',
+  'Hi',
+  'Bonjour',
+  'Ciao',
+  'Привет',
+  'こんにちは',
+  'Hei',
+  'Merhaba',
+  'Hallo',
+  'مرحبا'
+]
 
-    const currentGreeting = ref(0);
+const currentGreeting = ref(0)
 
-    const updateGreeting = () => {
-      currentGreeting.value = (currentGreeting.value + 1) % greetings.length;
-    };
-
-    let intervalId;
-
-    onMounted(() => {
-      intervalId = setInterval(updateGreeting, 3000); // Cambia cada 2 segundos
-    });
-
-    onUnmounted(() => {
-      clearInterval(intervalId);
-    });
-
-    return {greetings, currentGreeting}
-}
+const updateGreeting = () => {
+  const nextGreeting = currentGreeting.value + 1
+  currentGreeting.value = nextGreeting % GREETINGS.length
 }
 
+let intervalId
+
+onMounted(() => {
+  const INTERVAL_TIME = 3000
+
+  // Cambia cada 3 segundos
+  intervalId = setInterval(updateGreeting, INTERVAL_TIME)
+})
+
+onUnmounted(() => {
+  clearInterval(intervalId)
+})
 </script>
 
 <template>
-    <section class="greetings">
-        {{ greetings[currentGreeting] }}
-    </section>
+  <section class="greetings">
+    {{ GREETINGS[currentGreeting] }}
+  </section>
 </template>
 
 <style scoped>
-
-.greetings{
-    font-size: 64px;
-    line-height: auto;
-    font-weight: 600;
+.greetings {
+  font-size: 64px;
+  line-height: auto;
+  font-weight: 600;
 }
 
 @media (max-width: 900px) {
-
-.greetings{
+  .greetings {
     font-size: 50px;
-}
+  }
 }
 
 @media (max-width: 700px) {
-
-.greetings{
+  .greetings {
     font-size: 40px;
+  }
 }
-}
-
 </style>
