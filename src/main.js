0be76db9fba2a7bc60fe73cc89633edfec4c37aa
Cesar Sustaita <c.sustaita@outlook.com>
2024-09-19 12:01:58 -0600

    Add: Vercel Web Analytics

--- src/main.js
+++ src/main.js
@@ -6,6 +6,10 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import { inject } from '@vercel/analytics';
+
+inject();
+
 const app = createApp(App)
 
 app.use(createPinia())
